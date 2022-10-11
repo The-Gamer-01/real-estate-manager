f5c7be02595f53f49a1d778c49fc2acde26592d1
绿名蒟蒻 <843497509@qq.com>
2022-10-11 19:05:06 +0800

    创建gateway模块

---
+++ manage-gateway/src/main/java/com/hyx/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.hyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关模块启动类.
+ *
+ * @author hyx
+ **/
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
